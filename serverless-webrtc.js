0650c00f54944894613ea985c9c950e71e92f982
Robert Flack <flackr@gmail.com>
2019-12-10 14:12:14 -0500

    Add retransmission of chunks and profile selection.

--- serverless-webrtc.js
+++ serverless-webrtc.js
@@ -30,24 +30,67 @@ function getIceCandidates(connection) {
   });
 }
 
-const QUIET_PROFILE = 'ultrasonic-experimental';
-function transmit(data) {
+let QUIET_PROFILE = 'audible';
+const CHUNK_SIZE = 100;
+function transmitRaw(ab) {
   return new Promise((accept, reject) => {
     quietReady.then(() => {
-      let transmit = Quiet.transmitter({profile: QUIET_PROFILE, onFinish: accept});
-      let uncompressed = JSON.stringify(data);
-      let compressed = rtc_str_compress(uncompressed);
-      console.info('Compressed ' + uncompressed.length + ' down to ' + compressed.byteLength + ' bytes.');
-      transmit.transmit(compressed);
+      let transmitter = Quiet.transmitter({profile: QUIET_PROFILE, onFinish: accept});
+      let view = new Uint8Array(ab);
+      console.log('transmitting ' + ab.byteLength + ' bytes [' + view[0] + ', ' + view[1] + ', ' + view[2] + ', ' + view[3] + '...]');
+      transmitter.transmit(ab);
     });
   });
 }
 
-function receive() {
+const LONG_TIMEOUT = 500000;
+const TIMEOUT = 5000;
+async function transmit(data) {
+  let uncompressed = JSON.stringify(data);
+  let compressed = rtc_str_compress(uncompressed);
+  console.info('Compressed ' + uncompressed.length + ' down to ' + compressed.byteLength + ' bytes.');
+  let chunks = Math.ceil(compressed.byteLength / CHUNK_SIZE);
+  let description = new ArrayBuffer(6);
+  let bytes = new Uint8Array(description);
+  bytes[0] = bytes[1] = bytes[2] = chunks;
+  bytes[3] = bytes[4] = bytes[5] = compressed.byteLength % CHUNK_SIZE;
+  let success = false;
+  while (!success) {
+    await transmitRaw(description);
+    try {
+      response = new Uint8Array(await receiveRaw(0, TIMEOUT * 2.5));
+      success = response[0];
+    } catch (e) {}
+    if (!success) {
+      console.warn('Retransmitting header');
+    }
+  }
+  for (let i = 0; i < chunks; i++) {
+    console.log('sending chunk ' + i);
+    await transmitRaw(compressed.slice(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE));
+    let response = null;
+    while (!response) {
+      try {
+        response = new Uint8Array(await receiveRaw(0, LONG_TIMEOUT));
+      } catch (e) {}
+    }
+    if (response[0] <= i + 1) {
+      console.warn('Retransmitting ' + i);
+      chunks--;
+    }
+  }
+  console.log('All ' + chunks + ' chunks sent');
+}
+
+function receiveRaw(minBytes, timeout) {
   return new Promise((accept, reject) => {
     quietReady.then(() => {
       let content = new ArrayBuffer();
+      let timer;
+      let finished = false;
+      let receiver;
       let finish = (data, err) => {
+        clearTimeout(timer);
         receiver.destroy();
         if (err) {
           reject(err);
@@ -55,14 +98,24 @@ function receive() {
           accept(data);
         }
       }
-      let receiver = Quiet.receiver({profile: QUIET_PROFILE,
+
+      function timeoutfn() {
+        finish(null, 'Timeout');
+      }
+
+      console.log('awaiting ' + minBytes + ' bytes');
+      receiver = Quiet.receiver({profile: QUIET_PROFILE,
         onReceive: (payload) => {
+          if (finished)
+            return;
+          clearTimeout(timer);
           content = Quiet.mergeab(content, payload);
-          try {
-            let result = JSON.parse(rtc_str_decompress(content));
-            finish(result);
-          } catch (e) {
-            console.log('Receiving data, not valid JSON yet');
+          console.log('Got ' + content.byteLength + ' bytes');
+          if (content.byteLength >= minBytes) {
+            finished = true;
+            finish(content);
+          } else {
+            timer = setTimeout(timeoutfn, timeout);
           }
         },
         onCreateFail: (reason) => {
@@ -72,8 +125,54 @@ function receive() {
           finish(null, 'Failed to receive data');
         },
       });
+      timer = setTimeout(timeoutfn, timeout);
     });
   });
+}
+
+async function ack(value) {
+  let buffer = new ArrayBuffer(3);
+  let bytes = new Uint8Array(buffer);
+  bytes[0] = bytes[1] = bytes[2] = value;
+  await transmitRaw(buffer);
+}
+
+async function receive() {
+  // First receive the handshake
+  let header = null;
+  while (!header) {
+    try {
+      header = await receiveRaw(6, LONG_TIMEOUT);
+    } catch (e) {}
+  }
+  let headerBytes = new Uint8Array(header);
+  await ack(1);
+
+  // Now recieve the chunks
+  let chunks = headerBytes[0];
+  let lastSize = headerBytes[3];
+  let ab = new ArrayBuffer(0);
+  for (let i = 0; i < chunks; i++) {
+    console.log('awaiting chunk ' + i);
+    let expectedSize = i == chunks - 1 ? lastSize : CHUNK_SIZE;
+    let received = null;
+    while (!received) {
+      try {
+        received = await receiveRaw(expectedSize, TIMEOUT);
+        await ack(i + 2);
+      } catch (e) {
+        console.warn('Requesting retransmission of ' + i);
+        await ack(i + 1);
+      }
+    }
+    console.log('received chunk ' + i);
+    ab = Quiet.mergeab(ab, received);
+  }
+  console.log('All ' + chunks + ' chunks received');
+  // TODO: There's a risk that the sender didn't receive the last ack. We
+  // could try to confirm it but the same problem would exist in the other
+  // direction ¯\_(ツ)_/¯
+  return JSON.parse(rtc_str_decompress(ab));
 }
 
 Quiet.init({
